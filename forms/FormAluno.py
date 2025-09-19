c3aed0df4e274d8ca9f310b818a4a20aebdd994e
YannPG <yann_pg@hotmail.com>
2025-09-19 20:48:28 -0300

    feat: adicionar o modelo de FormAluno

---
+++ forms/FormAluno.py
@@ -0,0 +1,6 @@
+class FormAluno:
+    def __init__(self, codigo_aluno, nome, codigo_curso, codigo_cidade):
+        self.codigo_aluno = codigoAluno
+        self.nome = nome  
+        self.codigo_curso = codigoCurso
+        self.codigo_cidade = codigoCidade
