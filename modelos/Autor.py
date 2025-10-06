d6cbb18cd8749f88c094e3de47817ea187c53043
YannPG <yann_pg@hotmail.com>
2025-10-06 03:22:08 -0300

    feat: corrigir o model de autor

---
+++ modelos/Autor.py
@@ -0,0 +1,5 @@
+class Autor:
+    def __init__(self, codigo_autor, nome, codigo_cidade):
+        self.codigo_autor = codigo_autor
+        self.nome = nome
+        self.codigo_cidade = codigo_cidade 
--- modelos/Autores.py
+++
@@ -1,5 +0,0 @@
-class Autores:
-    def __init__(self):
-        self.codigo_autor = None
-        self.nome = None
-        self.codigo_cidade = None
