ba5424fa452e853c25e83b54390fa2519a4a1fc3
YannPG <yann_pg@hotmail.com>
2025-10-06 03:22:33 -0300

    feat: corrigir o model de categoria

---
+++ modelos/Categoria.py
@@ -0,0 +1,4 @@
+class Categoria:
+    def __init__(self, codigo_categoria, descricao):
+        self.codigo_categoria = codigo_categoria
+        self.descricao = descricao
--- modelos/Categorias.py
+++
@@ -1,4 +0,0 @@
-class Categorias:
-    def __init__(self):
-        self.codigo_categoria = None
-        self.descricao = None
