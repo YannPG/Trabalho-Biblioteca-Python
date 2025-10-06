25e38b9906b9f7620c11aae7dc8cbadccb8a7d08
YannPG <yann_pg@hotmail.com>
2025-10-06 03:23:04 -0300

    feat: corrigir o model de curso

---
+++ modelos/Curso.py
@@ -0,0 +1,4 @@
+class Curso:
+    def __init__(self, codigo_curso, descricao):
+        self.codigo_curso = codigo_curso
+        self.descricao = descricao 
--- modelos/Cursos.py
+++
@@ -1,5 +0,0 @@
-class Cursos:
-    def __init__(self):
-        self.codigo_curso = None
-        self.descricao = None
-        
