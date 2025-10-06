3cf93af1a0aa8ef5713f5dddb9b02fa55c39bbf1
YannPG <yann_pg@hotmail.com>
2025-10-06 03:23:23 -0300

    feat: corrigir o model de livro

---
+++ modelos/Livro.py
@@ -0,0 +1,8 @@
+class Livro:
+    def __init__(self, codigo_livro, titulo, codigo_autor, codigo_categoria, ano_publicacao, disponibilidade):
+        self.codigo_livro = codigo_livro
+        self.titulo = titulo
+        self.codigo_autor = codigo_autor 
+        self.codigo_categoria = codigo_categoria 
+        self.ano_publicacao = ano_publicacao 
+        self.disponibilidade = disponibilidade
--- modelos/Livros.py
+++
@@ -1,8 +0,0 @@
-class Livros:
-    def __init__(self):
-        self.codigo_livro = None
-        self.titulo = None
-        self.codigo_autor = None
-        self.codigo_categoria = None
-        self.ano_publicacao = None
-        self.disponibilidade = None
