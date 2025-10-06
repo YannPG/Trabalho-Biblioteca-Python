e216de323dccce802ee0975f94b44482043ff91e
YannPG <yann_pg@hotmail.com>
2025-10-06 03:22:46 -0300

    feat: corrigir o model de cidade

---
+++ modelos/Cidade.py
@@ -0,0 +1,5 @@
+class Cidade:
+    def __init__(self, codigo_cidade, descricao, estado):
+        self.codigo_cidade = codigo_cidade
+        self.descricao = descricao
+        self.estado = estado
--- modelos/Cidades.py
+++
@@ -1,6 +0,0 @@
-class Cidades:
-    def __init__(self):
-        self.codigo_cidade = None
-        self.descricao = None
-        self.estado = None
-    
