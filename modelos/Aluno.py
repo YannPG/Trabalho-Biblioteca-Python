1b1177ea50749b676c4e80383c8595a679e73043
YannPG <yann_pg@hotmail.com>
2025-10-06 03:21:50 -0300

    feat: corrigir a o model de aluno

---
+++ modelos/Aluno.py
@@ -0,0 +1,6 @@
+class Aluno:
+    def __init__(self, codigo_aluno, nome, codigo_curso, codigo_cidade):
+        self.codigo_aluno = codigo_aluno
+        self.nome = nome
+        self.codigo_curso = codigo_curso
+        self.codigo_cidade = codigo_cidade
